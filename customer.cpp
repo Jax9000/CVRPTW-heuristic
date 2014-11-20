f722b0157908ce5c0e59b72d795d8cab155b6422
Jax9000 <jax1994@gmail.com>
2014-11-20 22:18:36 +0100

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -16,9 +16,10 @@ Customer::Customer(int iid, int ix, int iy, int id, int ir, int istop, int iserv
     READY_TIME=ir;
     DUE_DATE=istop;
     SERVICE_TIME=iserv;
+    WYKONANY=0;
 }
 
 Customer::~Customer()
 {
-    cout << "destruktor" << endl;
+    
 }
